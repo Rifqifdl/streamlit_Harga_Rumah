4b00cabb7aaec5d2aa01f081669eb654807d0294
Rifqifdl <98653387+Rifqifdl@users.noreply.github.com>
2023-05-01 20:15:19 +0700

    Update estimasi-rumahdiparis.py

--- estimasi-rumahdiparis.py
+++ estimasi-rumahdiparis.py
@@ -5,11 +5,11 @@ model = pickle.load(open('estimasi_rumahdiparis.sav','rb'))
 
 st.title('Estimasi Harga Rumah di Paris')
 
-squareMeters = st.number_input('Input Luas Lahan')
+squareMeters = st.number_input('Input Luas Lahan (Meter Persegi)')
 numberOfRooms = st.number_input('Input Jumlah Ruangan')
 floors = st.number_input('Input Jumlah Lantai')
 numPrevOwners = st.number_input('Input Jumlah Pemilik Yang Pernah Menempati')
-basement = st.number_input('Input Luas Basement')
+basement = st.number_input('Input Luas Basement (Meter Persegi)')
 
 predict = ''
 
@@ -17,5 +17,5 @@ if st.button('Estimasi Harga'):
     predict = model.predict(
         [[squareMeters, numberOfRooms, floors, numPrevOwners, basement]]
     )
-    st.write ('Estimasi harga rumah dalam Dollar : ', predict)
-    st.write ('Estimasi harga rumah dalam IDR (Juta) :', predict*15000)
+    st.write ('Estimasi harga rumah dalam EUR : ', predict)
+    st.write ('Estimasi harga rumah dalam IDR :', predict*15000)
